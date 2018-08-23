0c755800f94978257269a26a019fdb77007cff45
TruongNM <manhtruonghedspi@gmail.com>
2018-08-23 15:17:35 +0700

    Login activity style animator

--- app/src/main/java/com/example/nguyenmanhtruong/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/nguyenmanhtruong/instagram/LoginActivity.kt
@@ -2,11 +2,45 @@ package com.example.nguyenmanhtruong.instagram
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
     private val TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        Log.d(TAG, "onCreate: ")
+
+        KeyboardVisibilityEvent.setEventListener(this, this)
+        login_btn.isEnabled = false
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
     }
+
+    override fun onVisibilityChanged(isOpen: Boolean) {
+        if (isOpen) {
+            scroll_view.scrollTo(0, scroll_view.bottom)
+            create_account_text.visibility = View.GONE
+        } else {
+            scroll_view.scrollTo(0, scroll_view.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        login_btn.isEnabled =
+                email_input.text.toString().isNotEmpty() &&
+                password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 }
