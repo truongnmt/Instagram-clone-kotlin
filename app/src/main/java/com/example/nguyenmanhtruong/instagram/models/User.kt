1d6809437d7e2db34b252e4793d10e1dd658f78c
TruongNM <manhtruonghedspi@gmail.com>
2018-08-23 16:18:26 +0700

    Sign in, sign out, pull user info to edit profile activity

---
+++ app/src/main/java/com/example/nguyenmanhtruong/instagram/models/User.kt
@@ -0,0 +1,4 @@
+package com.example.nguyenmanhtruong.instagram.models
+
+data class User(val name: String = "", val username: String = "", val website: String = "",
+                val bio: String = "", val email: String = "", val phone: Long = 0)
