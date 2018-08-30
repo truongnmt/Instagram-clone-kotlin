45af81e8aa0ba2d9c1f40f1ec643ae3f6d09d0f6
TruongNM <manhtruonghedspi@gmail.com>
2018-08-30 16:00:14 +0700

    Refator to package

--- app/src/main/java/com/example/nguyenmanhtruong/instagram/activities/Utils.kt
+++ app/src/main/java/com/example/nguyenmanhtruong/instagram/activities/Utils.kt
@@ -1,37 +1,14 @@
 package com.example.nguyenmanhtruong.instagram.activities
 
-import android.app.Activity
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
 import com.example.nguyenmanhtruong.instagram.R
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-
-class ValueEventListenerAdapter(val handler: (DataSnapshot) -> Unit) : ValueEventListener {
-
-    private val TAG = "ValueEventListenerAdapt"
-
-    override fun onDataChange(data: DataSnapshot) {
-        handler(data)
-    }
-
-    override fun onCancelled(error: DatabaseError) {
-        Log.e(TAG, "onCancelled: ", error.toException())
-    }
-
-}
-
-@GlideModule
-class CustomGlideModule : AppGlideModule()
+import com.example.nguyenmanhtruong.instagram.utils.GlideApp
 
 fun Context.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, duration).show()
