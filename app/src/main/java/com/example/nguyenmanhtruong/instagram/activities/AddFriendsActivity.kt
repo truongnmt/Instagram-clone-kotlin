4117b5c2047148fbf82c74ef3780a3e28540d790
TruongNM <manhtruonghedspi@gmail.com>
2018-09-05 11:03:04 +0700

    Add friend activity

---
+++ app/src/main/java/com/example/nguyenmanhtruong/instagram/activities/AddFriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nguyenmanhtruong.instagram.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.nguyenmanhtruong.instagram.R
+
+class AddFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+}
